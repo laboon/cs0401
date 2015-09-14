5d0fd8ef332b5063ed8d3a115099e13fcef8ef26
laboon <laboon@gmail.com>
2015-09-14 12:35:49 -0400

    scanner updates and if demo

---
+++ sample_code/IfDemo.java
@@ -0,0 +1,50 @@
+public class IfDemo {
+
+    // Plato defined "man" as a bipedal animal without feathers.
+    // This code will help us determine whether something is a man
+    // according to Plato.
+    // It could, of course, also be a bird or a griffin.
+    
+    // Note that Diogenes of Sinope later came to Plato's Symposium
+    // holding a plucked chicken and saying "Behold Plato's Man!"
+    
+    public static void main(String[] args) {
+
+	// || is OR
+	// && is AND
+	// ! is NOT
+	// Note - you can use ^ for XOR but it's technically a bitwise
+	// operator, so some people may look at your code askew.
+	// You can always do (a && !b) || (!a && b) instead
+	
+	boolean hasFeathers = true;
+	boolean hasTwoLegs = true;
+
+	if (hasFeathers || hasTwoLegs) {
+	    System.out.println("It is either a griffin or a bird.");
+	} else {
+	    System.out.println("It's not a griffin or a bird.");
+	}
+	
+	if (hasFeathers && hasTwoLegs) {
+	    System.out.println("it's a bird");
+	} else {
+	    System.out.println("it's not a bird");
+	}
+
+	if (hasFeathers && !hasTwoLegs) {
+	    System.out.println("it's a griffin");
+	} else {
+	    System.out.println("it's not a griffin");
+	}
+
+
+	if (!hasFeathers && hasTwoLegs) {
+	    System.out.println("Behold Plato's man!");
+	} else {
+	    System.out.println("It's not a man.");
+	}   
+
+    }
+
+}
--- sample_code/ScannerDemo.java
+++ sample_code/ScannerDemo.java
@@ -3,6 +3,9 @@
 
 import java.util.Scanner;
 
+// Ignore this for now
+import java.io.IOException;
+
 public class ScannerDemo {
 
     public static void main(String[] args) {
@@ -17,6 +20,17 @@ public class ScannerDemo {
 	System.out.print("Enter something, anything you want > ");
 	String someStr = sc.nextLine();
 	System.out.println("You typed " + someStr + "!");
+
+	// Use .next() to read in the next "token" on a line
+	// A token is a group of chars separated by whitespace -
+	// e.g. the string "12 cats love football!" has four
+	// token - "12", "cats", "love", and "football!"
+
+	System.out.print("Enter at least two tokens > ");
+	String str1 = sc.next();
+	String str2 = sc.next();
+	System.out.println("The first token is " + str1 + "!");
+	System.out.println("The second token is " + str2 + "!");
 	
 	// Read in an integer
 	// Note that an exception is thrown and the program
@@ -30,14 +44,52 @@ public class ScannerDemo {
 	
 	// You can send in multiple values as long as they are
 	// separated by whitespace (tabs, spaces, etc.)
-
+	// This will accept the *first two*.
+	// If you enter more, they are just "waiting" in the
+	// sc object.
+	
 	System.out.print("Enter two floats > ");
 	float f1 = sc.nextFloat();
 	float f2 = sc.nextFloat();
 	System.out.println("You typed " + f1 + " and " + f2 + "!");
 	System.out.println("Added together, that's " + (f1 + f2) + "!");
+
+	// Can we tell what the token is ahead of time? Yes!
+
+	System.out.println("Enter a token > ");
+	if (sc.hasNextInt()) {
+	    System.out.println("You typed an integer!");
+	    int anInt = sc.nextInt();
+	    System.out.println("It was " + anInt);
+	} else {
+	    System.out.println("You typed something other than an integer!");
+	    String aToken = sc.next();
+	    System.out.println("It was " + aToken);
+	}
 	
-	// Close the scanner.  It can no longer be used.
+	// Note that Scanner acts like a "wrapper" around System.in.
+	// We can use System.in directly, but it's more complicated.
+	// Note you don't have to understand the exception stuff yet.
+
+	System.out.println("Enter some other line > ");
+	int someCharValue = 0;
+	try {
+	    someCharValue = System.in.read();
+        } catch (IOException ioex) {
+	    System.exit(1);
+	}
+	char actualChar = (char) someCharValue;
+	System.out.println("The first char you typed was " + actualChar + "!");
+	System.out.println("That is character code " + someCharValue + "!");
+
+	// When using System.in and Scanner, you need to press enter to
+	// enter data, so "Press any key to continue..."-style
+	// code is not possible.  It's kind of silly, but you need
+	// to use yet another class, Console, or a third-party library
+	// like JLine to do that.
+	// That's beyond the scope of what we're discussing here, though.
+	
+	// Close the scanner.  After doing this, it can no longer be used.
 
 	sc.close();
 
