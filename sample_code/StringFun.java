9ba96c5f0e63aa3c3dc63de1b1fd7f0c11ae7b72
laboon <laboon@gmail.com>
2015-11-11 16:12:10 -0500

    Changes from class

--- sample_code/StringFun.java
+++ sample_code/StringFun.java
@@ -19,30 +19,43 @@ public class StringFun {
 	} catch (IndexOutOfBoundsException ex) {
 	    System.out.println("Not a lot of characters there, eh chief?");
 	}
-
+	
 	// .toLowerCase()
 	System.out.println("In all lowercase, your string is : " + s.toLowerCase());
 
 	// .toUpperCase()
 	System.out.println("In all uppercase, your string is : " + s.toUpperCase());
 
+	System.out.println("S is still " + s);
+	
 	// .substring
-	System.out.println("The first four characters of your string are : " + s.substring(0,3));
+	System.out.println("The first four characters of your string are : " + s.substring(0,4));
 	System.out.println("The third through last characters of your string are : " + s.substring(2));
-	System.out.println("The second through fourth characters of your string are : " + s.substring(1,3));
+	System.out.println("The second through fourth characters of your string are : " + s.substring(1,4));
 
+	
 	// .replace
 	System.out.println("Your string with all E's turned to 3s is: " + s.replace('E', '3')); // note chars, not strings
 	System.out.println("Your string with all E's and e's turned to 3s is: " + s.replace('E', '3').replace('e', '3'));
-	
+
+
+	System.out.println("Your string l337ified is: " + s.toUpperCase().replace('A', '4').replace('E', '3').replace('T', '7').replace('4', '@').replace('S', '$').replace('O', '0'));
+
 	// .indexOf
 	System.out.println("Location of first o is (-1 if none): " + s.indexOf('o'));
 
+	System.out.println("Location of first cat is (-1 if none): " + s.indexOf("cat"));
+	
 	// .lastIndexOf
 	System.out.println("Location of last o is (-1 if none): " + s.lastIndexOf('o'));
 
 	// .split
+	String[] ss = s.split(" ");
 	System.out.println("There are this many tokens separated by spaces: " + s.split(" ").length);
+	System.out.println("Tokens:");
+	for (String foo : ss) {
+	    System.out.println("\"" + foo + "\"");
+	}
 	
     }
 }
