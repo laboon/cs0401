10a0fc3dc26b9e96b8a95f33376886984eec6ac1
laboon <laboon@gmail.com>
2015-12-07 12:55:13 -0500

    add abstract demo

---
+++ sample_code/AbstractDemo.java
@@ -0,0 +1,68 @@
+public class AbstractDemo {
+
+    public static void main(String[] args) {
+	// Can't do this - abstract class
+	// Dog d = new Dog();
+
+
+	System.out.println("Chihuahua:");
+	Chihuahua c = new Chihuahua();
+	c.bark();
+	c.eat();
+
+	System.out.println("\nGreat Dane:");
+	GreatDane gd = new GreatDane();
+	gd.bark();
+	gd.eat();
+	
+	
+    }
+}
+
+// You cannot instantiate an abstract class!
+
+abstract class Dog {
+
+    // Methods that are _abstract_ must be filled in by subclasses
+    // before a subclass can be instantiated
+
+    // Kind of like an interface, no?
+    
+    public abstract void bark();
+
+    // However, regular old methods can co-exist (unlike pre-1.8
+    // interfaces).
+    
+    public void eat() {
+	System.out.println("Chomp chomp!");
+    }
+    
+}
+
+// Otherwise, though abstract classes act just like regular classes.
+// You can call super on them, have access to defined methods, etc.
+
+class Chihuahua extends Dog {
+
+    public void bark() {
+	System.out.println("Yip yip!");
+    }
+    
+}
+
+// Multiple subclasses are possible, just like regular classes
+
+class GreatDane extends Dog {
+
+    public void bark() {
+	System.out.println("Woof woof!");
+    }
+
+    public void eat() {
+	super.eat();
+	System.out.println("YUM YUM!");
+    }
+
+    
+}
+    
