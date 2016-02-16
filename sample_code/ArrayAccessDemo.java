5af2507df4e6cd50be74df7826eca0c42b1f39de
laboon <laboon@gmail.com>
2016-02-16 10:53:05 -0500

    update sequential vs direct

--- sample_code/ArrayAccessDemo.java
+++ sample_code/ArrayAccessDemo.java
@@ -1,28 +1,56 @@
 public class ArrayAccessDemo {
 
-    public static void sequentialAccess(int[] arr) {
-	for (int val : arr) {
-	    
-    }
-
-    public static void directAccess(int[] arr) {
-	for (int j=0; j < arr.length; j++) {
-	    
+    // This is just a helper method I made to pretty-print
+    // arrays.
+    
+    public static void printArray(int[] arr) {
+	System.out.print("[ ");
+	for (int j=0; j < (arr.length - 1); j++) {
+		System.out.print(arr[j] + ", ");
 	}
-
 	
+	System.out.println(arr[arr.length - 1] + " ]");
+
     }
+
     
     public static void main(String[] args) {
 	System.out.println("Accessing the index 5 of array... ");
-	int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int[] arr = { 14, 21, 1, 17, 42, 39, 18, 12, 8, 10 };
 
-	// Direct access here, too.  Read/write
+	// Direct access here -> we do not need to go through
+	// positions 0 through 4 to get here!
 
-	arr[0] = arr[1];
+	arr[5] = 0;
+
+	printArray(arr);
 	
-	sequentialAccess(arr));
-	System.out.println("direct: " + directAccess(arr));
+	// Sequential access -> you need to go through each
+	// previous element to get to a certain place
+	// In this situation, we are looking for the value
+	// 8 and if we find it, replace it with -1.
+	// Since we don't know where 8 is, we can't directly
+	// access it - we need to sequentially search through
+	// all the elements.
+	
+	for (int j=0; j < arr.length; j++) {
+	    if (arr[j] == 8) {
+		arr[j] = -1;
+	    }
+	}
+	
+	printArray(arr);
+	
+	// Why would you want to do sequential access instead
+	// of direct access?  It seems so much more work.
+	// 1. Some data types (e.g. lists) can only be accessed
+	// sequentially
+	// 2. Necessary for, say, searching an unsorted array
+	// 3. Necessary for accessing an entire array
+
+	// Let's say you are looking for 
+	
+	
     }
     
 }
